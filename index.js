1c6ec4e64696627fc3a671cf3d71ec92eeceaf6d
alexey-ernest <alexey.ernest@gmail.com>
2016-08-11 02:44:14 +0300

    Add or append logic

--- index.js
+++ index.js
@@ -32,26 +32,60 @@ bus.on('frame', function (msg) {
     {name: 'TEXT_DETECTION', maxResults: 10}
   ];
 
+  function finishProcessing(frame) {
+    debug('Video frame Google recognition results (' + frame.uri + '): ' + JSON.stringify(frame));
+    msg.finish();
+  }
+
   recognizer.detect(frame.uri, {features: features}, function (err, results) {
     if (err) return handleError(err);
 
-    // save recognition results
-    Frame.create({
-      videoId: frame.videoId,
-      videoUri: frame.videoUri,
-      segmentIndex: frame.segmentIdx,
-      frameIndex: frame.frameIdx,
-      uri: frame.uri,
-      tags: results[0].labels,
-      text: results[0].text,
-      logos: results[0].logos,
-      faces: results[0].faces
-    }, function (err, f) {
-      if (err) return handleError(err);
-      debug('Video frame recognition results (' + frame.uri + '): ' + JSON.stringify(f));
-
-      msg.finish();
+    // checking if the frame has been alreade processed by another analyzer
+    var query  = Frame.where({
+      videoId: frame.videoId, 
+      segmentIndex: frame.segmentIdx, 
+      frameIndex: frame.frameIdx
     });
+
+    query.findOne(function (err, frame) {
+      if (err) return handleError(err);
+
+      // save recognition results
+      if (frame) {
+        debug('Appending Google recognition results to the existing frame record.');
+
+        // update frame
+        var updateParams = {
+          tags: results[0].labels,
+          text: results[0].text,
+          logos: results[0].logos,
+          faces: results[0].faces
+        };
+        frame.update(updateParams, function (err) {
+          if (err) return handleError(err);
+
+          finishProcessing(frame);
+        });
+      } else {
+        // create frame
+        Frame.create({
+          videoId: frame.videoId,
+          videoUri: frame.videoUri,
+          segmentIndex: frame.segmentIdx,
+          frameIndex: frame.frameIdx,
+          uri: frame.uri,
+          tags: results[0].labels,
+          text: results[0].text,
+          logos: results[0].logos,
+          faces: results[0].faces
+        }, function (err, f) {
+          if (err) return handleError(err);
+
+          finishProcessing(f);
+        });
+      }
+    });
+
   });
 });
 
